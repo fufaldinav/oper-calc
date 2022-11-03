9b4314fcd5de2a885b10e602e166719a150196b5
Artem Fufaldin <artem.fufaldin@gmail.com>
2022-11-04 04:56:06 +0500

    Initial

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <RouterView></RouterView>
+</template>
+
+<script>
+import { RouterView } from "vue-router";
+
+export default {
+  components: { RouterView },
+
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss"></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <main>
+    <h1 class="text-3xl font-bold">Hello World!</h1>
+  </main>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss"></style>
